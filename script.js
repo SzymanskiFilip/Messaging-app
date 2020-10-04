705c33f185e160372fdff57eba9bf6008b0aa4ae
Filip Szymanski <szymanski.fp@gmail.com>
2020-10-04 15:53:42 +0200

    changed the textbox value after sending the message

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ button.addEventListener("click", function(){
     var newMessage = document.createElement("li");
     newMessage.innerHTML = textbox.value;
     messages.appendChild(newMessage);
+    textbox.value = "";
 });
