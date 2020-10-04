1e2f2b46b78a6f5f63f8a47c6cd2233c5808492a
Filip Szymanski <szymanski.fp@gmail.com>
2020-10-04 15:48:19 +0200

    connected variables to html elements

---
+++ script.js
@@ -0,0 +1,7 @@
+var messages = document.getElementById("messages");
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+
+});
