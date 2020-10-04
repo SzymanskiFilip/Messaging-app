303aa1f3aa1641121fedfcf244981be030749102
Filip Szymanski <szymanski.fp@gmail.com>
2020-10-04 15:51:56 +0200

    added a eventlistener which adds a li element to the ul with the message if clicked

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ var textbox = document.getElementById("textbox");
 var button = document.getElementById("button");
 
 button.addEventListener("click", function(){
-
+    var newMessage = document.createElement("li");
+    newMessage.innerHTML = textbox.value;
+    messages.appendChild(newMessage);
 });
